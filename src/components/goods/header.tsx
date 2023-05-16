c25d447e41715bffa75a6492e6049f42575748fd
Poul Sunny <siesta.cloud@gmail.com>
2023-05-16 22:59:32 +0300

    ok

--- src/components/goods/header.tsx
+++ src/components/goods/header.tsx
@@ -1,22 +1,21 @@
-import { useState, useEffect } from "react";
 import { Logo } from "../logo/logo";
-import { useToggle } from "../../hooks/useToggle";
 import { useScroll } from "../../hooks/useScroll";
 import { BurgerMenu } from "../burger/burger";
+import { Search } from "../search/search";
 
 
 function Header() {
 
+  
   const [ScrollState] = useScroll();
-
 
   return (
     <header className="header">
       <div className={`header__wrapper ${ScrollState ? "scrolled" : ""}`}>
         <Logo logo="HitShop" />
+        <Search></Search>
         <BurgerMenu/>
       </div>
-
     </header>
   );
 }
